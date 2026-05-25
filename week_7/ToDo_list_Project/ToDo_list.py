a9d8b2a26c4444e2796d2cfd7d9cd59d63a6e913
Mendel <MendelCohen770@gmail.com>
2026-05-25 10:59:34 +0300

    add function save tasks

--- week_7/ToDo_list_Project/ToDo_list.py
+++ week_7/ToDo_list_Project/ToDo_list.py
@@ -13,3 +13,10 @@ def load_tasks(file_name: str) -> list:
     return tasks_lst
 
 
+def save_tasks(file_name: str, tasks:list[dict]):
+    with open(file_name, "a", encoding="utf-8") as f:
+        for task in tasks:
+            temp_line = f"{task["id"]} | {task["status"]} | {task["description"]}\n"
+            f.write(temp_line)
+
+
