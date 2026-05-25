7a7c5efa01c6c3b67acd47bf588cb4b7e427d52c
Mendel <MendelCohen770@gmail.com>
2026-05-25 11:29:41 +0300

    add function add task

--- week_7/ToDo_list_Project/ToDo_list.py
+++ week_7/ToDo_list_Project/ToDo_list.py
@@ -4,8 +4,8 @@ def load_tasks(file_name: str) -> list:
     tasks_lst = []
     try:
         with open(file_name, "r", encoding="utf-8") as f:
-            for line in f.readline():
-                task_id, task_status, task_description = line.split("|")
+            for line in f:
+                task_id, task_status, task_description = line.strip().split("|")
                 temp_dict = {"id":task_id, "status": task_status, "description": task_description}
                 tasks_lst.append(temp_dict)
     except FileNotFoundError as e:
@@ -16,7 +16,18 @@ def load_tasks(file_name: str) -> list:
 def save_tasks(file_name: str, tasks:list[dict]):
     with open(file_name, "a", encoding="utf-8") as f:
         for task in tasks:
-            temp_line = f"{task["id"]} | {task["status"]} | {task["description"]}\n"
+            temp_line = f"{task["id"]}|{task["status"]}|{task["description"]}\n"
             f.write(temp_line)
+
+
+def add_task(file_name, description):
+    old_tasks = load_tasks(file_name)
+    next_id = int(old_tasks[-1]["id"]) + 1
+    temp_line = f"{next_id}|pending|{description}\n"
+
+    with open(file_name, "a", encoding="utf-8") as f:
+        f.write(temp_line)
+
+add_task("note.txt", "hello")
 
 
