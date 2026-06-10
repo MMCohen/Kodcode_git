bfe2b91cf9b8050b8a4680b2fe711b5239562fd2
Mendel <MendelCohen770@gmail.com>
2026-06-10 11:49:29 +0300

    add get summary func

--- week_9/SQL/day_4/SQL_Aggregation.py
+++ week_9/SQL/day_4/SQL_Aggregation.py
@@ -8,3 +8,30 @@ def get_connector():
         password="secret",
         database="soldiers_db",
     )
+
+
+def get_summary():
+    """
+    :return: dict with total soldiers, active soldiers and inactive soldiers
+    """
+    data_dict = dict()
+
+    connector = get_connector()
+    cursor = connector.cursor(dictionary=True)
+
+    cursor.execute("select count(*) as total from soldiers;")
+    data_dict.update(cursor.fetchone())
+
+    cursor.execute("select count(*) as active from soldiers where active = 1;")
+    data_dict.update(cursor.fetchone())
+
+    cursor.execute("select count(*) as inactive from soldiers where active = 0;")
+    data_dict.update(cursor.fetchone())
+
+    cursor.close()
+    connector.close()
+
+    return data_dict
+
+if __name__ == "__main__":
+    print(get_summary())
